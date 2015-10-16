34e726c91b7a07d01b084f3d11b0a8285722969f
ilovezfs <ilovezfs@icloud.com>
2015-10-16 03:13:27 -0700

    Bump version to 1.4.4

--- scripts/make-pkg.sh
+++ scripts/make-pkg.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version=1.4.3
+version=1.4.4
 owner=`logname`
 dev_id_application="Developer ID Application: Joergen  Lundman (735AM5QEU3)"
 dev_id_installer="Developer ID Installer: Joergen  Lundman (735AM5QEU3)"
