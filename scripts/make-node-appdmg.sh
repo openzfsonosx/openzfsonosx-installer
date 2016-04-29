eeb5e0211ce7d82c607789327d45175dcd937722
ilovezfs <ilovezfs@icloud.com>
2016-04-29 00:30:47 -0700

    WIP: node appdmg

--- scripts/make-node-appdmg.sh
+++ scripts/make-node-appdmg.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+source "$PWD"/version
+
+APPDMG=/usr/local/bin/appdmg
+JSON_PATH="$PWD"/appdmg/assets/appdmg.json
+DMG_PATH="$PWD"/appdmg-$version-$(date +%s).dmg
+
+cp $JSON_PATH".in" $JSON_PATH
+perl -p -i -e "s/\@version\@/$version/g" $JSON_PATH
+
+$APPDMG $JSON_PATH $DMG_PATH
+rm $JSON_PATH
+
 exit 0
