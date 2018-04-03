618448a05bb407caba18984c9ad7c43808d6d400
ilovezfs <ilovezfs@icloud.com>
2018-04-03 01:37:00 -0700

    Bump version to 1.7.3

--- make-installers.sh
+++ make-installers.sh
@@ -107,8 +107,8 @@ SIERRADESTDIR="${SIERRAPAK}"/1012
 HIGHSIERRAPAK="${topdir}"/packages-o3x-1013
 HIGHSIERRADESTDIR="${HIGHSIERRAPAK}"/1013
 
-SPL_TAG=spl-1.7.2
-ZFS_TAG=zfs-1.7.2
+SPL_TAG=spl-1.7.3
+ZFS_TAG=zfs-1.7.3
 
 if [ $make_only -eq 1 ]
 then
